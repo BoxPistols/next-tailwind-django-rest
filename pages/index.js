faf0be66f115d16da060a86afda255702b3a8cd2
aito <BoxPistols@users.noreply.github.com>
2022-04-23 01:58:07 +0900

    setting on Mac

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,22 @@ import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Layout title="Home" h1="Hell?" />
-            <footer className={styles.footer}>
-                <a href="#" rel="noopener noreferrer">
-                    Powered by{' '}
-                    <span className={styles.logo}>
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            width={72 / 2}
-                            height={16 / 2}
-                        />
-                    </span>
-                </a>
-            </footer>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <Layout title="Home" h1="Hell?" />
+      <footer className={styles.footer}>
+        <a href="#" rel="noopener noreferrer">
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              width={72 / 2}
+              height={16 / 2}
+            />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
 }
