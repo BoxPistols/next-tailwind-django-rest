caf8d3af645a16385cbcaeba23eaa9ab9e0d93d2
ito.a <ax@ax.in>
2022-04-29 22:30:24 +0900

    getProp Base

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { getAllPostData } from '../lib/posts'
 
 const Blog = () => {
-  return (
-    <div>
-      <Layout title="Blog">blog page</Layout>
-    </div>
-  )
+    return (
+        <div>
+            <Layout title="Blog">blog page</Layout>
+        </div>
+    )
+}
+
+// ビルド時に１回だけサーバーサイドでレンダリングされる
+export async function getAllPostData() {
+    const posts = await getAllPostData()
 }
 
 export default Blog
